28122cd51918f59cde636044cd4766e0abf9da68
Guilherme Ivo <guilhermeivob@gmail.com>
2021-08-07 03:39:19 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleMath.UnitTests/CalculadoraTest.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+
+namespace ConsoleMath.UnitTests
+{
+    public class CalculadoraTest
+    {
+        [Test]
+        public void ShouldReturnCorrectSum()
+        {
+            int firstValue = 10;
+            int secondValue = 20;
+
+            var result = new Calculator().SimpleMathCalculation(
+                firstValue, 
+                secondValue, 
+                OperationEnum.addition);
+
+            Assert.AreEqual(30, result);
+        }
+
+        [Test]
+        public void ShouldReturnCorrectSubtraction()
+        {
+            int firstValue = 20;
+            int secondValue = 10;
+
+            var result = new Calculator().SimpleMathCalculation(
+                firstValue, 
+                secondValue, 
+                OperationEnum.subtraction);
+
+            Assert.AreEqual(10, result);
+        }
+
+
+        [Test]
+        public void ShouldReturnCorrectDivision()
+        {
+            int firstValue = 20;
+            int secondValue = 10;
+
+            var result = new Calculator().SimpleMathCalculation(
+                firstValue, 
+                secondValue, 
+                OperationEnum.division);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void ShouldReturnCorrectMultiplication()
+        {
+            int firstValue = 10;
+            int secondValue = 20;
+
+            var result = new Calculator().SimpleMathCalculation(
+                firstValue, 
+                secondValue, 
+                OperationEnum.multiplication);
+
+            Assert.AreEqual(200, result);
+        }
+    }
+}
---
+++ src/ConsoleMath/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleMath
+{
+    public class Calculator
+    {
+        public int SimpleMathCalculation(int firstValue, int secondValue, OperationEnum operation)
+        {
+            switch (operation)
+            {
+                case OperationEnum.addition:
+                    return firstValue + secondValue;
+                case OperationEnum.subtraction:
+                    return firstValue - secondValue;
+                case OperationEnum.division:
+                    return firstValue / secondValue;
+                case OperationEnum.multiplication:
+                    return firstValue * secondValue;
+                default:
+                    throw new Exception("Entered operation is incorrect");
+            };
+        }
+    }
+}
